fd3ea62423ac7f186265bba31d7ebe9b4078b093
Seth Vargo <sethvargo@gmail.com>
2016-09-21 12:13:23 -0400

    Update build scripts

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"os"
 )
 
-const Name = "atlas-upload"
-const Version = "0.2.0"
-
 func main() {
 	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
 	os.Exit(cli.Run(os.Args))
---
+++ version.go
@@ -0,0 +1,6 @@
+package main
+
+const (
+	Name    = "atlas-upload"
+	Version = "0.2.0"
+)
